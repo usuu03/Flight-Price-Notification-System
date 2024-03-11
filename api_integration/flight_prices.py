e006699747b46e8f3a46aee4348561c499df06c6
Usu <usuedeaghe@gmail.com>
2024-03-11 16:34:08 +0000

    Get Flight price details function implementation

--- api_integration/flight_prices.py
+++ api_integration/flight_prices.py
@@ -2,12 +2,12 @@ import requests
 
 URL = "https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights"
 
-def get_flight_price(source_airport_code, destination_airport_code, departure_date, number_people):
+def get_flight_price(source_airport_code, destination_airport_code, departure_date, number_people, return_date=None):
     querystring = {
         "sourceAirportCode": source_airport_code,
         "destinationAirportCode": destination_airport_code,
         "date": departure_date,
-        "itineraryType": "ONE_WAY",
+        "itineraryType": "ONE_WAY" if return_date is None else "ROUND_TRIP",
         "sortOrder": "PRICE",
         "numAdults": number_people,
         "numSeniors": "0",
@@ -15,6 +15,9 @@ def get_flight_price(source_airport_code, destination_airport_code, departure_da
         "pageNumber": "1",
         "currencyCode": "GBP"
     }
+
+    if return_date:
+        querystring['returnDate'] = return_date
 
     headers = {
         "X-RapidAPI-Key": "ce4025f581msh671d0d550d83460p17ee11jsn201673161249",
@@ -26,25 +29,28 @@ def get_flight_price(source_airport_code, destination_airport_code, departure_da
     data = response.json()
 
     if data.get('status'):
-        session_data = data['data']['session']
         flights_data = data['data']['flights']
-
-        print("Session:")
-        print(f"\tSearch Hash: {session_data['searchHash']}")
-        print(f"\tPage Load UID: {session_data['pageLoadUid']}")
-        print(f"\tSearch ID: {session_data['searchId']}")
 
         print("Flights:")
         for flight in flights_data:
             print("\t---")
-            print(f"\tDeparture: {flight['segments'][0]['legs'][0]['originStationCode']}")
-            print(f"\tDestination: {flight['segments'][-1]['legs'][-1]['destinationStationCode']}")
-            print(f"\tDeparture Date: {flight['segments'][0]['legs'][0]['departureDateTime']}")
-            print(f"\tArrival Date: {flight['segments'][-1]['legs'][-1]['arrivalDateTime']}")
-            print(f"\tMarketing Carrier: {flight['segments'][0]['legs'][0]['marketingCarrier']['displayName']}")
-            print(f"\tTotal Price: {flight['purchaseLinks'][0]['totalPrice']}")
+            for segment in flight['segments']:
+                print("\tSegment:")
+                for leg in segment['legs']:
+                    print(f"\t\tDeparture: {leg['originStationCode']}")
+                    print(f"\t\tDestination: {leg['destinationStationCode']}")
+                    print(f"\t\tDeparture Date: {leg['departureDateTime']}")
+                    print(f"\t\tArrival Date: {leg['arrivalDateTime']}")
+                    print(f"\t\tMarketing Carrier: {leg['marketingCarrier']['displayName']}")
+                print("\t---")
+            print(f"\tTotal Price: £{flight['purchaseLinks'][0]['totalPrice']}")
             print(f"\tURL: {flight['purchaseLinks'][0]['url']}")
+
+            # Print return date if it exists
+            if 'returnDate' in querystring:
+                print(f"\tReturn Date: {querystring['returnDate']}")
     else:
         print("No flights found.")
 
-get_flight_price("MAD", "LHR", "2024-04-25", 1)
+# Example usage
+get_flight_price("LGW", "AGP", "2024-04-11", 1)

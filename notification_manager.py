93e717752b8b5380fb3096db5af2334812f6a29c
Usu <usuedeaghe@gmail.com>
2024-03-20 14:19:04 +0000

    sms integration

--- notification_manager.py
+++ notification_manager.py
@@ -1,22 +1,30 @@
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-
-TWILIO_SID = "*************"
-TWILIO_TOKEN = "************"
-TWILIO_VIRTUAL_NUM = "******"
-TWILIO_NUM = "********"
-
+# Load environment variables from .env file
+load_dotenv()
 
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
+    # This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.client = Client(TWILIO_SID, TWILIO_TOKEN)
+        # Get Twilio credentials from environment variables
+        twilio_sid = os.getenv("TWILIO_SID")
+        twilio_token = os.getenv("TWILIO_TOKEN")
+        twilio_virtual_num = os.getenv("TWILIO_VIRTUAL_NUM")
+        twilio_num = os.getenv("TWILIO_NUM")
+
+        # Initialize Twilio client
+        self.client = Client(twilio_sid, twilio_token)
+        self.twilio_virtual_num = twilio_virtual_num
+        self.twilio_num = twilio_num
 
     def send_sms(self, message):
+        # Send SMS using Twilio
         message = self.client.messages.create(
             body=message,
-            from_=TWILIO_VIRTUAL_NUM,
-            to=TWILIO_NUM,
+            from_=self.twilio_virtual_num,
+            to=self.twilio_num,
         )
-        # Prints if successfully sent.
+        # Print if successfully sent
         print(message.sid)
